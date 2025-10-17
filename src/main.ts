b57c3e56e8197c6beb68f32268dedec7125e252b
Steel Maxima <steelmaxima21@gmail.com>
2025-10-17 21:08:58 +0100

    added cors functionality

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:["*"],
+    methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials:true
+  })
   app.useGlobalInterceptors(new LoggingInterceptor())
   await app.listen(process.env.PORT ?? 3005);
 }
